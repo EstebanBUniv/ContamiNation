777faec0ab6b38f438d89867a620891d17d29db8
unknown <jennyrichard72000@gmail.com>
2026-06-01 15:38:22 +0200

    ajout de la classe Plateau

---
+++ ContamiNation/Metier/Plateau.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Plateau
+{
+    private final ArrayList<Sommets> SOMMETS = new ArrayList<Sommets>();
+   
+    private int largeur;
+    private int hauteur;
+    private int nbCouleur;
+    private String[] couleurs;
+    private int tailleCases;
+    private Case[] tabCases;
+
+    public Plateau(int largeur, int hauteur, int nbCouleur)
+    {
+        this.largeur   = largeur;
+        this.hauteur   = hauteur;
+        this.nbCouleur = nbCouleur;
+
+        this.couleurs    = new String[this.nbCouleur];
+        this.tailleCases = 50;
+        this.tabCases    = new Case[this.largeur * this.hauteur];
+    }
+
+    public Case getCase(int indice)
+    {
+        return this.tabCases[indice];
+    }
+    
+    
+}
