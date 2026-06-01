87feb3430d2ca0d6bf6153e0a9ce984dec6da0a9
unknown <jennyrichard72000@gmail.com>
2026-06-01 15:56:07 +0200

    ajout de methode toString

--- ContamiNation/Metier/Plateau.java
+++ ContamiNation/Metier/Plateau.java
@@ -11,7 +11,18 @@ public class Plateau
 	private int tailleCases;
 	private Case[][] tabCases;
 
-	public Plateau(int largeur, int hauteur, int nbCouleur)
+	private void creaCase()
+    {
+        for (int i = 0; i<this.hauteur; i++ )
+        {
+            for(int j = 0; j < this.largeur; j++)
+            {
+                this.tabCases[i][j] = new Case(i,j);
+            }
+        }
+    }
+    
+    public Plateau(int largeur, int hauteur, int nbCouleur)
 	{
 		this.largeur   = largeur;
 		this.hauteur   = hauteur;
@@ -19,13 +30,30 @@ public class Plateau
 
 		this.couleurs    = new String[this.nbCouleur];
 		this.tailleCases = 50;
-		this.tabCases    = new Case[this.largeur][this.hauteur];
+		this.tabCases    = new Case[this.hauteur][this.largeur];
+        this.creaCase();
 	}
 
 	public Case getCase(int x, int y)
 	{
 		return this.tabCases[x][y];
 	}
+
+    public String toString()
+    {
+        String res = "";
+
+        for (int i = 0; i<this.hauteur; i++ )
+        {
+            for(int j = 0; j < this.largeur; j++)
+            {
+                res += this.tabCases[i][j] ;
+            }
+            res += "\n";
+        }
+        
+        return res;
+    }
 	
 
 }
