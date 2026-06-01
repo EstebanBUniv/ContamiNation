93ec25b365f0c4576800f065a29fd547b1bc875f
EstebanBidaux <esteban.bidaux@gmail.com>
2026-06-01 15:47:11 +0200

    Modification d'un tableau 2d pour les cases

--- ContamiNation/Metier/Plateau.java
+++ ContamiNation/Metier/Plateau.java
@@ -2,30 +2,30 @@ import java.util.ArrayList;
 
 public class Plateau
 {
-    private final ArrayList<Sommet> SOMMETS = new ArrayList<Sommet>();
+	private final ArrayList<Sommet> SOMMETS = new ArrayList<Sommet>();
    
-    private int largeur;
-    private int hauteur;
-    private int nbCouleur;
-    private String[] couleurs;
-    private int tailleCases;
-    private Case[] tabCases;
+	private int largeur;
+	private int hauteur;
+	private int nbCouleur;
+	private String[] couleurs;
+	private int tailleCases;
+	private Case[][] tabCases;
 
-    public Plateau(int largeur, int hauteur, int nbCouleur)
-    {
-        this.largeur   = largeur;
-        this.hauteur   = hauteur;
-        this.nbCouleur = nbCouleur;
+	public Plateau(int largeur, int hauteur, int nbCouleur)
+	{
+		this.largeur   = largeur;
+		this.hauteur   = hauteur;
+		this.nbCouleur = nbCouleur;
 
-        this.couleurs    = new String[this.nbCouleur];
-        this.tailleCases = 50;
-        this.tabCases    = new Case[this.largeur * this.hauteur];
-    }
+		this.couleurs    = new String[this.nbCouleur];
+		this.tailleCases = 50;
+		this.tabCases    = new Case[this.largeur][this.hauteur];
+	}
 
-    public Case getCase(int indice)
-    {
-        return this.tabCases[indice];
-    }
-    
+	public Case getCase(int x, int y)
+	{
+		return this.tabCases[x][y];
+	}
+	
 
 }
