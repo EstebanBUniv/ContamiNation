e1d1fba9bce601b90b9c8793c9bdfdc7114b3624
EstebanBidaux <esteban.bidaux@gmail.com>
2026-06-01 16:05:56 +0200

    Ajout d'une classe test

---
+++ ContamiNation/Metier/Test.java
@@ -0,0 +1,46 @@
+import iut.algo.*;
+
+public class Test
+{
+	public static void main (String[] args)
+	{
+		int largeur, hauteur, nbCouleur;
+		System.out.println("Donnez la largeur puis hauteur puis le nombre de couleur");
+		largeur = Clavier.lire_int();
+		hauteur = Clavier.lire_int();
+		nbCouleur = Clavier.lire_int();
+		
+		Plateau plateau = new Plateau(largeur, hauteur, nbCouleur);
+		System.out.println(plateau);
+		
+		boolean continuer = true;
+		while (continuer)
+		{
+			System.out.println("Voulez-vous continuer ?");
+			continuer = Clavier.lireString().equals("oui");
+			
+			int x, y;
+			System.out.println("Coordonnées de la case : ");
+			x = Clavier.lire_int() - 1;
+			y = CLavier.lire_int() - 1;
+			
+			plateau.getCase(x,y).ajouterZone(1);
+			System.out.println(plateau);
+		}
+		
+		continuer = true;
+		while (continuer)
+		{
+			System.out.println("Voulez-vous continuer ?");
+			continuer = Clavier.lireString().equals("oui");
+			
+			int x, y;
+			System.out.println("Coordonnées de la case : ");
+			x = Clavier.lire_int() - 1;
+			y = CLavier.lire_int() - 1;
+			
+			plateau.getCase(x,y).ajouterSommet("Test");
+			System.out.println(plateau);
+		}
+	}
+}
