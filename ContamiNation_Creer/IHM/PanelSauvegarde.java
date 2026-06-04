a3ba3807922422e17d0c7655dc13cbc284eebf56
Rafael Courtois <rafael.courtois@etu.univ-lehavre.fr>
2026-06-04 15:48:13 +0200

    correction bug actionPerformed

--- ContamiNation_Creer/IHM/PanelSauvegarde.java
+++ ContamiNation_Creer/IHM/PanelSauvegarde.java
@@ -125,13 +125,14 @@ public class PanelSauvegarde extends JPanel implements ActionListener
 				this.fichiersDossier.get(ligne).delete();
 				this.rafraichir();
 			}
-		}
-
-		if (e.getSource() == this.btnRenommer)
-			
+		}			
 		
 		if (e.getSource() == this.btnModifier)
+		{
+			System.out.println("modif 1");
 			this.frameMere.charger(this.fichiersDossier.get(ligne));
+		}
+			
 			
 
 		if (e.getSource() == this.btnNouveau)
