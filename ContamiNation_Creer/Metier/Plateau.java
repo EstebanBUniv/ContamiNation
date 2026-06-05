898bc41c6b17f1a4fe643400db48c5dfaa519cc9
Gregory Theard <gregory.theard72100@gmail.com>
2026-06-05 14:18:54 +0200

    Changement bouton Zone

--- ContamiNation_Creer/Metier/Plateau.java
+++ ContamiNation_Creer/Metier/Plateau.java
@@ -97,10 +97,6 @@ public class Plateau
 		return this.lstVirus.get(nomVirus).getNom();
 	}
 
-	public String getNomVirus(int numVirus)
-	{
-		return this.lstVirus.get(numVirus).getNom();
-	}
 
 	/*----------------------------*/
 	/*  Setters                   */
